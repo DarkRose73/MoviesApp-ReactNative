547013be3ad869e86b4eecdf4466d42210f18987
Joan Gabriel Salas Silva <joan.salas.silva@gmail.com>
2023-01-13 10:27:26 -0300

    MovieCard component

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,10 +1,42 @@
-import {View, Text} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
 import React from 'react';
+import {Movie} from '../interfaces/movieInterface';
 
-export const MovieCard = () => {
+interface Props {
+  movie: Movie;
+}
+
+export const MovieCard = ({movie}: Props) => {
+  const uri = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+
   return (
-    <View>
-      <Text>MovieCard</Text>
+    <View
+      style={{
+        width: 300,
+        height: 420,
+      }}>
+      <View style={styles.imageContainer}>
+        <Image source={{uri}} style={styles.image} />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+    borderRadius: 20,
+  },
+  imageContainer: {
+    flex: 1,
+    borderRadius: 18,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.37,
+    shadowRadius: 7.49,
+    elevation: 10,
+  },
+});
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,9 +2,11 @@ import {View, Text, ActivityIndicator} from 'react-native';
 import React from 'react';
 import {useMovies} from '../hooks/useMovies';
 import {MovieCard} from '../components/MovieCard';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
   const {moviesNowPlaying, isLoading} = useMovies();
+  const {top} = useSafeAreaInsets();
 
   if (isLoading) {
     return (
@@ -19,8 +21,8 @@ export default function HomeScreen() {
     );
   }
   return (
-    <View>
-      <MovieCard></MovieCard>
+    <View style={{marginTop: top + 20}}>
+      <MovieCard movie={moviesNowPlaying[0]}></MovieCard>
     </View>
   );
 }
