96f84b0d027c9f8a15fd340ec3fcc4053950dffd
Joan Gabriel Salas Silva <joan.salas.silva@gmail.com>
2023-01-16 16:08:14 -0300

    GradientBackground

---
+++ src/components/GradientBackground.tsx
@@ -0,0 +1,21 @@
+import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+import LinearGradient from 'react-native-linear-gradient';
+
+interface Props {
+  children: JSX.Element | JSX.Element[];
+}
+
+export const GradientBackground = ({children}: Props) => {
+  return (
+    <View style={{flex: 1}}>
+      <LinearGradient
+        colors={['#084F6A', '#75CEDB', 'white']}
+        style={{...StyleSheet.absoluteFillObject}}
+        start={{x: 0.1, y: 0.1}}
+        end={{x: 0.5, y: 0.8}}
+      />
+      {children}
+    </View>
+  );
+};
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -6,6 +6,7 @@ import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 import Carousel from 'react-native-snap-carousel';
 import {HorizontalSlider} from '../components/HorizontalSlider';
+import {GradientBackground} from '../components/GradientBackground';
 
 const {width: windowWidth} = Dimensions.get('window');
 
@@ -26,27 +27,29 @@ export default function HomeScreen() {
     );
   }
   return (
-    <ScrollView>
-      <View style={{marginTop: top + 20}}>
-        {/* Principal Carousel */}
-        <View style={{height: 440, paddingTop: 5}}>
-          <Carousel
-            data={nowPlaying}
-            itemWidth={300}
-            sliderWidth={windowWidth}
-            inactiveSlideOpacity={0.9}
-            renderItem={({item}) => <MovieCard movie={item}></MovieCard>}
-            autoplay={true}
-            autoplayInterval={7000}
-            loop={true}
-          />
-        </View>
+    <GradientBackground>
+      <ScrollView>
+        <View style={{marginTop: top + 20}}>
+          {/* Principal Carousel */}
+          <View style={{height: 440, paddingTop: 5}}>
+            <Carousel
+              data={nowPlaying}
+              itemWidth={300}
+              sliderWidth={windowWidth}
+              inactiveSlideOpacity={0.9}
+              renderItem={({item}) => <MovieCard movie={item}></MovieCard>}
+              autoplay={true}
+              autoplayInterval={7000}
+              loop={true}
+            />
+          </View>
 
-        {/* Trending movies */}
-        <HorizontalSlider title="Popular" movies={popular} />
-        <HorizontalSlider title="Top rated" movies={topRated} />
-        <HorizontalSlider title="Upcoming" movies={upcoming} />
-      </View>
-    </ScrollView>
+          {/* Trending movies */}
+          <HorizontalSlider title="Popular" movies={popular} />
+          <HorizontalSlider title="Top rated" movies={topRated} />
+          <HorizontalSlider title="Upcoming" movies={upcoming} />
+        </View>
+      </ScrollView>
+    </GradientBackground>
   );
 }
